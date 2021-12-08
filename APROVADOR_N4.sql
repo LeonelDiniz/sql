51a414b02d98cfcd7db9ca542bb05d9e0b2fc6d7
leonel.diniz@apexbrasil.com.br <leonel.diniz@apexbrasil.com.br>
2021-12-08 12:02:16 -0300

    select aprovadores

---
+++ APROVADOR_N4.sql
@@ -0,0 +1,8 @@
+select * from protheus_apex.sx6010
+where d_e_l_e_t_ = ' '
+and x6_var like 'AP_GRPAPN%';
+
+SELECT * FROM PROTHEUS_APEX.vw_usrcc
+WHERE USR_CODIGO LIKE 'tatian%';
+
+SELECT * FROM PROTHEUS_APEX.ZZ5010
